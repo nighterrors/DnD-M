11392c5baf0fb983791c912f50de3d95588106b4
nighterrors <65488887+nighterrors@users.noreply.github.com>
2022-10-30 20:58:45 +0000

    FEAT(barter): Add functions to calc. haggled price
    Signed-off-by: nighterrors <65488887+nighterrors@users.noreply.github.com>

--- scripts/dice.js
+++ scripts/dice.js
@@ -88,6 +88,36 @@ Sums the elements of an array. The array should contain numbers.
 	);
 }
 
+	//Barter
+
+function FfPricePaidByShop(IfMarketValue,IiShopDicePool,IiPlayerDicePool) {
+/*
+Calculates a haggled price for an item *sold to a shop*.
+	**`IfMarketValue`**:	*Should be float!* What's listed in the rule book. (Or usually half of it, if it's used.)
+	**`IiShopDicePool`**:	*Should be int!* The shopkeeper's haggling potential. - E.g.: their **Persuasion modifier**.
+	**`IiPlayerDicePool`**:	*Should be int!* The player's haggling potential. - E.g.: their **Persuasion modifier**.
+	**Returns**:			*Float* The adjusted price the shopkeeper -currently being interacted with- would pay for the specific item.
+							(In the same currency that the original price was given as the 1st argument.)
+*/
+	let LfPricePaidByShop = ( IfMarketValue * ( 111 - FfSum(FaGenDiceRolls(2,20)) - FfSum(FaGenDiceRolls(IiPlayerDicePool,10)) + FfSum(FaGenDiceRolls(IiShopDicePool,10)) ) / 100 );
+
+	return LfPricePaidByShop;
+}
+
+function FfPricePaidByPlayer(IfMarketValue,IiShopDicePool,IiPlayerDicePool) {
+	/*
+	Calculates a haggled price for an item *bought from a shop*.
+		**`IfMarketValue`**:	*Should be float!* What's listed in the rule book.
+		**`IiShopDicePool`**:	*Should be int!* The shopkeeper's haggling potential. - E.g.: their **Persuasion modifier**.
+		**`IiPlayerDicePool`**:	*Should be int!* The player's haggling potential. - E.g.: their **Persuasion modifier**.
+		**Returns**:			*Float* The adjusted price the shopkeeper -currently being interacted with- would pay for the specific item.
+								(In the same currency that the original price was given as the 1st argument.)
+	*/
+		let LfPricePaidByPlayer = ( IfMarketValue * ( 89 + FfSum(FaGenDiceRolls(2,20)) + FfSum(FaGenDiceRolls(IiPlayerDicePool,10)) - FfSum(FaGenDiceRolls(IiShopDicePool,10)) ) / 100 );
+	
+		return LfPricePaidByPlayer;
+	}
+		
 function FaGenSkills() {
 /*
 Generates an array of raw values, which can be assigned to the 6 skills.
